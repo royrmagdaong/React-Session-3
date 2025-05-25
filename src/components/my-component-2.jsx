3d5832ad5f5b67ebbfbf88a9e76eff7e6afc1cb3
roy <royrmagdaong@gmail.com>
2025-05-25 14:19:10 +0800

    inline css

--- src/components/my-component-2.jsx
+++ src/components/my-component-2.jsx
@@ -1,11 +1,13 @@
 // import react from 'react'
 
-function MyComponent2(){
+function MyComponent2(props){
     // pascal case MyComponent
     // camecase myComponent
+
+    // alt + z
     return (
         <div>
-            <h1>My component 2</h1>
+            <h1 style={{color:'red', backgroundColor: '#00ff00'}}>hello {props.name}</h1>
         </div>
     )
 }
