85b04d5faac6aa89e3a858c1c6d37c6c3e1ff36f
roy <royrmagdaong@gmail.com>
2025-05-25 16:27:31 +0800

    fetch 3rd party api using fetch

--- src/components/parent.jsx
+++ src/components/parent.jsx
@@ -1,6 +1,6 @@
 import ChildOne from "./child-1";
 import ChildTwo from "./child-2";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Parent = () => {
     const [name, setName] = useState('john')
@@ -12,6 +12,28 @@ const Parent = () => {
         console.log(msg)
         setMessage(msg)
     }
+
+    useEffect( ()=>{
+
+        fetchRandomJoke()
+    },[])
+
+    const fetchRandomJoke = async () => {
+        // fetch 3rd party api // synchronous and asynchronous
+        // using fetch built in js
+        // another way is to use axios
+        
+        const response = await fetch("https://icanhazdadjoke.com/slack");
+        if (!response.ok) {
+            console.log('error')
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json)
+    }
+    
+
 
     return (
         <div style={{backgroundColor: '#eee'}}>
