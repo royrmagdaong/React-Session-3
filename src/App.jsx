29c568350ed90c6b75b993e6f56e1542402faae1
roy <royrmagdaong@gmail.com>
2025-05-25 14:32:39 +0800

    managing state, comparing common var to component state using useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import MyComponent from './components/MyComponent'
 import MyComponent2 from './components/my-component-2'
 
 function App() {
+  const name="Peter"
 
   return (
     <>
-      <div>Hello World!!!</div>
-      <MyComponent name="Peter" />
-      <MyComponent2 />
+      {/* <div>Hello World!!!</div> */}
+      <MyComponent name={name} />
+      {/* <MyComponent2 name="John" /> */}
     </>
   )
 }
--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,17 +1,34 @@
 // import react from 'react'
+import { useState } from "react"
 
 function MyComponent(props){
+    const [count, setCount] = useState(0)
+    let count2 = 1
 
+    // count = 2 error
     // props.name = "John" ERROR - read only
+
+    // setCount(1)
 
     return (
         <div>
             {/* <h1>
                 hello {props.name}
             </h1> */}
-            <h1>
+            {/* <h1>
                 hello {props["name"]}
-            </h1>
+            </h1> */}
+            <div>
+                {count}
+                <br/>
+                <button onClick={()=>{setCount(count + 1)}}>counter</button>
+            </div>
+
+            <div>
+                {count2}
+                <br/>
+                <button onClick={()=>{count2++; console.log(count2)}}>counter 2</button>
+            </div>
         </div>
     )
 }
